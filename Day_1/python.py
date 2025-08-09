b4a0b75919243e2837d942c8324c15f9725e8b4c
Muhammad Haseeb <muhammadhaseebahmed09@gmail.com>
2025-08-09 11:38:05 -0700

    Day2 learning

---
+++ Day_1/python.py
@@ -0,0 +1,11 @@
+
+
+age = 22
+name = "Haseeb"
+height = 5.7
+is_programmer = True
+fav_fruits = ["Apple","Orange","Melon","Cherry"]
+personal_info = {"name":"haseeb","age":22,"Qualification":"BS Genetics"}
+
+print(f"My name is {name}. My age is {age}. My height is {height}.")
+print(f"{personal_info[name]}")
